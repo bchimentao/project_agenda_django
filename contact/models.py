158a74dee65c486c635954016c0a9b54244f03b1
bchimentao <bchimentao@yahoo.com.br>
2024-03-08 14:47:09 -0300

    adding models, gitignore, media and static in urls and settings

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+# after any changes here:
+# python manage.py makemigrations  --> create the files for migration
+# python manage.py migrate         --> migrate
+
 #id (primary key - automatic)
+
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50) #Charfield is for strings
@@ -10,6 +16,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True) #blank=True allows optional filling
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
