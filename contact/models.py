682ebe4dd9e947745041b918af1634ed73ef0d8a
bchimentao <bchimentao@yahoo.com.br>
2024-03-08 11:15:22 -0300

    creating class contact and changing language and timezone

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#id (primary key - automatic)
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50) #Charfield is for strings
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True) #blank=True allows optional filling
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
