974c5a771c383e59ccfb8c141644abd3f09d040d
bchimentao <bchimentao@yahoo.com.br>
2024-04-16 09:45:28 -0300

    adding search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 
 from contact.models import Contact
 
@@ -10,6 +11,34 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip() # get the text in the search, if doesn't, get nothing
+
+    if search_value == '':
+        return redirect('contact:index')
+
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id') # icontains check if the value is in the text (case-insensitive)
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
     }
 
     return render(
@@ -34,3 +63,4 @@ def contact(request, contact_id):
         'contact/contact.html',
         context,
     )
+
