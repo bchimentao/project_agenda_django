5009c87cd40f4312747c25b4b3f6aea4beb0f226
bchimentao <bchimentao@yahoo.com.br>
2024-04-16 10:19:02 -0300

    pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 from contact.models import Contact
 
 def index(request):
     contacts = Contact.objects\
         .filter(show=True)\
-        .order_by('-id')[0:10]
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)  # Show 10 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -35,9 +41,13 @@ def search(request):
             Q(email__icontains=search_value)
         )\
         .order_by('-id') # icontains check if the value is in the text (case-insensitive)
+    
+    paginator = Paginator(contacts, 10)  # Show 10 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
